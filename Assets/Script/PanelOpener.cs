2a0bcd9621e9bd817b20e4adb274e29ca0aff732
Floorban <15696082812@163.com>
2023-06-01 06:16:57 +0200

    good luck with test

--- Assets/Script/PanelOpener.cs
+++ Assets/Script/PanelOpener.cs
@@ -1,9 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
+using TMPro;
 public class PanelOpener : MonoBehaviour
+{
+    public GameObject Photo;
+    public GameObject BOpen;
+    public GameObject BClose;
+    private bool _isOpen = false;
+    private Textbox textbox;
+
+    public AudioSource soundPlayer;
+
+    private void Start()
+    {
+        Photo.SetActive(false);
+        BOpen.SetActive(false);
+        BClose.SetActive(false);
+        textbox = GetComponent<Textbox>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !_isOpen)
+        {
+            BOpen.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Photo.SetActive(false);
+            BOpen.SetActive(false);
+            BClose.SetActive(false);
+        }
+    }
+    public void OnCloseButtonClick()
+    {
+        if (Photo != null)
+        {
+            bool isActive = Photo.activeSelf;
+            Photo.SetActive(!isActive);
+            
+        }
+
+            textbox.NextSentence();
+            soundPlayer.Play();
+
+    }
+}
+/*public class PanelOpener : MonoBehaviour
 {
     public GameObject Panel;
     public GameObject Info;
@@ -45,4 +93,4 @@ public class PanelOpener : MonoBehaviour
         }
 
     }
-}
+}*/
