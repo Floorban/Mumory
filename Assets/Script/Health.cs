78d5265263cf925ba97262017fa86ac8d8973d21
Floorban <15696082812@163.com>
2023-06-08 12:28:40 +0200

    health and postprocessing

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Health : MonoBehaviour
 {
     //public QTE2Script qte2Script;
-
+    public Volume postProcessingVolume;
+    private Bloom _bloom;
     public Text healthText;
     public Image healthBar;
 
@@ -16,7 +19,16 @@ public class Health : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        health = 50;
+        health = 40;
+        if (postProcessingVolume.profile.TryGet(out _bloom))
+    {
+        // Successfully retrieved the Bloom component
+        // Now you can use _bloom to modify the bloom intensity
+    }
+    else
+    {
+        Debug.LogWarning("Bloom component not found in the post-processing volume.");
+    }
     }
 
     // Update is called once per frame
@@ -30,7 +42,36 @@ public class Health : MonoBehaviour
         HealthBarFiller();
         ColorChanger();
 
+        if (health >= 0)
+        {
+            _bloom.intensity.value = 1f;
+        }
 
+           if (health >= 10)
+        {
+            _bloom.intensity.value = 2f;
+        }
+
+           if (health >= 20)
+        {
+            _bloom.intensity.value = 3f;
+        }
+
+           if (health >= 30)
+        {
+            _bloom.intensity.value = 4f;
+        }
+
+           if (health >= 40)
+        {
+            _bloom.intensity.value = 5f;
+        }
+
+           if (health >= 50)
+        {
+            _bloom.intensity.value = 6f;
+        }
+     
     }
 
     void HealthBarFiller()
